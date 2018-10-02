3c79acfecc59412bccb59faa247999b03daa2a6f
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-10-02 12:40:19 -0500

    built schedule course component and grid finished

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -8,11 +8,11 @@ class LibraryCourse extends Component {
             <div className="library-course">
                 <div className="libray-course__title-check">
                     <label className="library-course__title">Problem Solving</label>
-                    {Icon('fas fa-check', 'library-course__icon')}
+                    {Icon("fas fa-check", "library-course__icon")}
                 </div>
                 <Arrow className="library-course__arrow" />
                 <Action className="library-course__action" />
-                {Icon('fas fa-plus-circle', 'libray-course__icon')}
+                {Icon("fas fa-plus-circle", "libray-course__icon")}
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Hersey cok guzel olacak sadece biraz sabir Sahismail. Biliyorum bazi seyler zor senin icin ama sadece birazcik sabret aslanim</p>

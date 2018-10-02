d640633e61646230728a0b29821411a4e1e6c7ba
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-10-02 11:02:49 -0500

    buildlibrary course component

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                {/* icon component */}
+                {/* arrow component */}
+                {/* action button component */}
+                <div className="library-course__description">
+                    <label>Course Description</label>
+                    <p>Hersey cok guzel olacak sadece biraz sabir Sahismail. Biliyorum bazi seyler zor senin icin ama sadece birazcik sabret aslanim</p>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default LibraryCourse;

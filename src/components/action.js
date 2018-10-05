8fe0fd9731b930cb3e0105c48f6f9e0d7462539d
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-10-05 11:22:15 -0500

    styled action component

--- src/components/action.js
+++ src/components/action.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
+
+    handleAction = function () {
+        this.props.onClick()
+    }.bind(this);
+
     render() {
         return (
-            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                {Icon('fas fa-plus-circle')}
-                {/* fas fa-times-circle */}
-            </button>
+            <a
+                id="action"
+                onClick={() => this.handleAction()}
+                className={`${this.props.className} action`}>
+            </a>
         )
     }
+
 }
 
+
 export default Action;
+
+// return (
+        //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+        //         {Icon('fas fa-plus-circle')}
+        //         {/* fas fa-times-circle */}
+        //     </button>
+        // )
